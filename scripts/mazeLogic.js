ca7654c4a8f1d1490aba3f04d08fd8e4bb3c6f03
Alexxx180 <alexx120702@gmail.com>
2021-10-31 14:48:45 +0300

    Alpha 1.12 | Grid templating

--- scripts/mazeLogic.js
+++ scripts/mazeLogic.js
@@ -11,20 +11,6 @@ var startTime = [1, 0];
 var time = [startTime[0],  startTime[1]];
 var timer;
 
-function Rectangle ( X,Y,DX,DY, R1,G1,B1, R2,G2,B2 ) {
-	var canvas = document.getElementById('Image1');
-	var Image1Canvas = canvas.getContext('2d');
-	Image1Canvas.beginPath();
-	Image1Canvas.fillStyle =  'rgb('+R1+','+G1+','+B1+')';
-	Image1Canvas.fillRect(X,Y,DX,DY);
-	Image1Canvas.stroke();
-	Image1Canvas.beginPath();
-	Image1Canvas.lineWidth = 1;
-	Image1Canvas.strokeStyle = 'rgb('+R2+','+G2+','+B2+')';
-	Image1Canvas.strokeRect(X,Y,DX,DY);
-	Image1Canvas.stroke();
-	};
-
 function PoiskPoLuchu( n ) {
 	var i,X1,Y1,NomKol,NomStr;
 	Rez = [ -1,-1,500 ];
@@ -88,7 +74,7 @@ function Step( n ) {
 			};
 		};
 	return Rez;
-	};
+}
 
 function Svobodno(nomKol, nomStr) {
 	var Rez = 1, i, k;
@@ -113,15 +99,15 @@ function Pobeda(nomKol, nomStr) {
 
 function Button1Click() {
 	if (Button1.value == 'Играть') {
-		document.getElementById('panel1').style.left = '-2000px';
+		document.getElementById('panel1').style.visibility = "hidden";
 		Button1.value = 'Справка';
 		Pause(false);
 	} else {
-		document.getElementById('panel1').style.left = '15px';
+		document.getElementById('panel1').style.visibility = "visible";
 		Button1.value = 'Играть';
 		Pause(true);
 	}
-};
+}
 
 function Button2Click() {
 	if (pause)
@@ -135,7 +121,7 @@ function Button2Click() {
 		X = X1;
 		Y = Y1;
 		if (Pobeda(NomKol, NomStr)) {
-			document.getElementById('wonInfo').style.left = '0px';
+			document.getElementById('wonInfo').style.visibility = "visible";
 		}
 		}
 	PokazScen();
@@ -146,7 +132,7 @@ function Button3Click() {
 		return;
 	Fi = Fi - 0.05;
 	PokazScen();
-	};
+}
 
 function Button4Click() {
 	if (pause)
@@ -161,23 +147,23 @@ function Button4Click() {
 		Y = Y1;
 		}
 	PokazScen();
-	};
+}
 
 function Button5Click() {
 	if (pause)
 		return;
 	Fi = Fi + 0.05;
 	PokazScen();
-	};
+}
 
 function Reload() {
 	X = 395;
 	Y = 215;
-	document.getElementById('wonInfo').style.left = '-2000px';
-	document.getElementById('loseInfo').style.left = '-2000px';
+	document.getElementById('wonInfo').style.visibility = "hidden";
+	document.getElementById('loseInfo').style.visibility = "hidden";
 	time = [startTime[0],  startTime[1]];
 	document.getElementById('Timer1').innerText = TimeText();
-	document.getElementById('panel1').style.left = '15px';
+	document.getElementById('panel1').style.visibility = "visible";
 	Button1.value = 'Играть';
 	Pause(true);
 	Fi = -Math.PI/2;
@@ -208,7 +194,7 @@ function Timer() {
 	if (time[1] > 0)
 		timer = setTimeout(Timer, 1000);
 	else {
-		document.getElementById('loseInfo').style.left = '0px';
+		document.getElementById('loseInfo').style.visibility = "visible";
 	}
 }
 
@@ -243,16 +229,9 @@ function FormCreate() {
 	var i,H;
 	for(i=0;i<KolKol;i++){
 		document.getElementById("Walls").innerHTML += '<div id="K'+i+'" style="position:absolute; left:'+(i*2)+'px; top:100px; width:2px; height:770px; background:rgb(200,100,100); "></div>';
-		};
+	}
 	document.getElementById("Timer1").innerText = TimeText();
 	document.onkeydown = Klav;
-	Rectangle(0,0,1800,1000, 0,0,0, 0,0,0);
-	for(i=0;i<500;i++){
-			H = (500-i)/1000; 
-			Rectangle(0,     i,1800,1, 0,0,0, 255*H,255*H,255*H);
-			Rectangle(0,1000-i,1800,1, 0,0,0, 100*H, 50*H,  0*H);
-		};
 	PokazScen();
-
-	};
+}
 //var time = 10;
